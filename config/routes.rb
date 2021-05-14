42d1c14fddd4dcda6b9974e7b942f601efd4deb0
Franx0 <francisco.moya.martinez@gmail.com>
2021-05-14 22:12:06 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     scope module: :v1, constraints: ApiVersion.new('v1', true) do
-
+      get '/history', to: 'histories#index'
+      post '/calc', to: 'histories#create'
     end
   end
 end
