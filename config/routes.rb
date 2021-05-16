de02b115485e6d4576680a3454f2c4c6821670e2
Franx0 <francisco.moya.martinez@gmail.com>
2021-05-16 13:46:21 +0200

    add react app route router on routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,18 @@ require "./lib/api/version"
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'app#index'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     scope module: :v1, constraints: ApiVersion.new('v1', true) do
       get '/history', to: 'histories#index'
       post '/calc', to: 'histories#create'
     end
   end
+
+  # React App routes
+  root 'app#index'
+
+  # Redirect missing routes to react client, always at bottom of file
+  match '*path', to: 'app#index', via: :all, constraints: lambda { |req|
+    req.path.exclude? 'rails/active_storage'
+  }
 end
