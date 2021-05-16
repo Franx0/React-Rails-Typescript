4da48fa10bac05c5f4d9499950a2cd5e686fe2ea
Franx0 <francisco.moya.martinez@gmail.com>
2021-05-16 13:46:31 +0200

    update histories controller && spec

--- app/controllers/api/v1/histories_controller.rb
+++ app/controllers/api/v1/histories_controller.rb
@@ -1,7 +1,7 @@
 module Api::V1
   class HistoriesController < BaseController
     def index
-      render json: { data: History.all }
+      render json: { data: formatted_response(History.all) }
     end
 
     def create
@@ -10,7 +10,7 @@ module Api::V1
       if @history.errors.any?
         render json: { errors: @history.errors.messages }, status: :unprocessable_entity
       else
-        render json: { data: @history.calc }, status: :created
+        render json: { data: formatted_response(@history) }, status: :created
       end
     end
 
@@ -18,6 +18,16 @@ module Api::V1
 
     def permitted_params
       params.require(:history).permit(:typed, :data => [])
+    end
+
+    def formatted_response(data)
+      res = lambda { |history| { history: history, calculation: history.calc } }
+
+      if (data.is_a? ActiveRecord::Relation)
+        data.map{|h| res.call(h)}
+      else
+        res.call(data)
+      end
     end
   end
 end
